df552046c0baac467473961156850dd0daba6d37
Rafael Batista <rafaelsbatista@gmail.com>
2016-01-07 19:30:04 -0200

    First service with express api

--- app.js
+++ app.js
@@ -1,24 +1,14 @@
-//function statement
+var express = require('express');
+var app = express();
 
-function greet() {
-	console.log('hi');
-}
+var port = process.env.PORT || 3000;
 
-greet();
+app.get('/', function(req, res) {
+	res.send('<html><head></head><body><h1>Hello world!</h1></body></html>');
+});
 
-// function are first-class
-function logGreenting(fn) {
-	fn();
-}
+app.get('/api', function(req, res) {
+	res.json({ firstname: 'John' , lastname: 'Doe' });
+});
 
-logGreenting(greet);
-
-
-// function expression
-var greetMe = function() {
-	console.log("Hi Rafael");
-}
-
-greetMe();
-
-logGreenting(greetMe);
+app.listen(port);
