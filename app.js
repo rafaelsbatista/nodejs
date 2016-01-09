2a808a5e47f13dd85e1206974f67071a000aeefa
Rafael Batista <rafaelsbatista@gmail.com>
2016-01-09 21:18:34 -0200

    Commit NodeJs + AngularJs

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
 var express = require('express');
 var app = express();
+var fs = require('fs');
 
 var port = process.env.PORT || 3000;
+
+app.set('view engine', 'ejs');
 
 app.use('/assets', express.static(__dirname + '/public'));
 
 app.use('/', function (req, res, next){
-	console.log('Request Url: ' + req.url);
+	//console.log('Request Url: ' + req.url);
 	next();
 });
 
 app.get('/', function(req, res) {
-	res.send('<html><head><link href=assets/style.css type=text/css rel=stylesheet /></head><body><h1>Hello world!</h1></body></html>');
+	res.render('index');
 });
 
 app.get('/person/:id', function(req, res) {
