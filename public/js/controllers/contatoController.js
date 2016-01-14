7ec061f9541f759051688c536267b65162268a90
Rafael Batista <rafaelsbatista@gmail.com>
2016-01-14 21:00:06 -0200

    layout change

--- public/js/controllers/contatoController.js
+++ public/js/controllers/contatoController.js
@@ -1,3 +1,9 @@
 myApp.controller('contatoController', ['$scope', '$rootScope', function($scope, $rootScope) {
     $rootScope.$emit('tabChange', 3);
+    
+    var modal = $('.modal-backdrop');
+    
+    if (modal) {
+        modal.remove();
+    }
 }]);
