a433bc36db27be54b9dd5dda54f302cdd560019e
Rafael Batista <rafaelsbatista@gmail.com>
2016-01-22 00:10:06 -0200

    Adding youtube embed video

--- public/js/controllers/mainController.js
+++ public/js/controllers/mainController.js
@@ -3,6 +3,26 @@ myApp.controller('mainController', ['$scope', '$rootScope', function($scope, $ro
     
     var modal = $('.modal-backdrop');
     
+    $scope.videoUrl = 'https://www.youtube.com/watch?v=IPrOlrYHsoQ';
+
+    $scope.$on('youtube.player.playing', function ($event, player) {
+         $('.carousel').carousel('pause');
+        console.log("playing");
+    });
+    $scope.$on('youtube.player.paused', function ($event, player) {
+        // play it again
+         $('.carousel').carousel('cycle');
+    });
+    $scope.$on('youtube.player.ended', function ($event, player) {
+        // play it again
+         $('.carousel').carousel('cycle');
+    });
+
+
+    $scope.changeClick = function() {
+        $('.carousel').carousel('cycle');
+    }
+    
     if (modal) {
         modal.remove();
     }
